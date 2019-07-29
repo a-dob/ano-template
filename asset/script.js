9b1b8cde37723c7f1a6ff350f05e20986f48d25f
Anatoly Dobychin <31900584+a-dob@users.noreply.github.com>
2019-07-29 17:21:55 +0300

    last

--- asset/script.js
+++ asset/script.js
@@ -1,10 +1,23 @@
+
 $(document).ready(function(){
+  //подсветка найденных совпадений
+  function highlightOverlap(needle, content){
+      regexp = new RegExp(needle, "ig");
+      newContent = content.replace(regexp, '<span class="lightOn">' + needle + '</span>');
+      //newContent = '<i class="fa fa-plus" aria-hidden="true"></i>' + newContent;
+      return newContent;
+  }
+
+
   var tree = $('.tree');
   var tree_link = $('.tree a');
+  var tree_ul = $('.tree ul');
   tree_link.on('click', function(){
     $(this).next().toggleClass( "collapsed");
     $(this).find('i').eq(0).toggleClass('fa-minus');
   });
+
+
 
   var step1 = $('#step1');
   var step2 = $('#step2');
@@ -25,5 +38,28 @@ $(document).ready(function(){
     });
     $(this).addClass('active');
   });
+
+
+  //tree search
+  var treeSearch = $('#treeSearch');
+  treeSearch.on('keyup', function(){
+    tree_ul.addClass('collapsed');
+    tree.find('i').removeClass('fa-minus');
+
+    var search = $(this).val();
+    tree.find('a').each(function(){
+      if (~$(this).text().toLowerCase().indexOf(search.toLowerCase())){
+        $(this).parents('ul').removeClass('collapsed');
+        $(this).parents('ul').each(function(){
+          $(this).parent().find('i').addClass('fa-minus');
+        });
+        $(this).html(highlightOverlap(search, $(this).text()));
+        //console.log($(this).text());
+      }
+    })
+  });
+
+
+  //tree search
 
 });
